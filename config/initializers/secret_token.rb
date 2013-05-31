3e44feac5979552fbe4331966647a4b7e74c6bbf
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-05-31 14:05:49 +0100

    Blank Rails 3.2.13 app
    rails new fact-cave -T -J -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FactCave::Application.config.secret_token = '724d14fe9db27ff1b8dc095d72f6313615cfc2645f29abc80ad3fbad5be9b0f8bc38fd3bb04315313cf4035b61881059ba5b9d0f81312c3c49c6b4577ca4850a'
