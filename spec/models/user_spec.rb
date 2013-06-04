0bf33e612daaf5e37d719f729398cdaf9cc2accd
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-06-04 14:58:59 +0000

    Add gds-sso attribute test for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = FactoryGirl.create(:user,
+                               :uid => '123factmeister',
+                               :name => 'Factmeister General',
+                               :email => 'factmeister@digital.cabinet-office.gov.uk',
+                               :permissions => ['fact-whisperer'])
+  end
+  it "should have gds-sso attributes" do
+    @user.uid.should == '123factmeister'
+    @user.name.should == 'Factmeister General'
+    @user.email.should == 'factmeister@digital.cabinet-office.gov.uk'
+    @user.permissions.should == ['fact-whisperer']
+  end
 end
