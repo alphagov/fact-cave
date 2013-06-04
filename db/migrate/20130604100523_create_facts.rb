9bc07bf5945b84b1cdad07865a4345b31ac1babd
Kushal Pisavadia <kushi.p@gmail.com>
2013-06-04 17:28:40 +0100

    Fields for the 'fact' table should be NOT NULL
    We don't want null fields creeping into our database. Just means that
    our database (schema) validates our data model as well as the Rails
    models we're using here.

--- db/migrate/20130604100523_create_facts.rb
+++ db/migrate/20130604100523_create_facts.rb
@@ -1,10 +1,10 @@
 class CreateFacts < ActiveRecord::Migration
   def change
     create_table :facts do |t|
-      t.string :name
-      t.string :slug
-      t.text :description
-      t.string :value
+      t.string :name, :null => false
+      t.string :slug, :null => false
+      t.text :description, :null => false
+      t.string :value, :null => false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(:version => 20130604104805) do
 
   create_table "facts", :force => true do |t|
-    t.string   "name"
-    t.string   "slug"
-    t.text     "description"
-    t.string   "value"
+    t.string   "name",        :null => false
+    t.string   "slug",        :null => false
+    t.text     "description", :null => false
+    t.string   "value",       :null => false
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
