1ad6bda2f0264996fe0c2b14745da02ce8df4791
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-06-05 10:21:28 +0100

    Rename and merge duplicated feature specs

--- spec/features/facts_spec.rb
+++ spec/features/facts_spec.rb
@@ -1,17 +1,25 @@
 require 'spec_helper'
 
-feature "getting a single fact" do
-  it "should have an HTTP status of OK" do
+feature "serving facts" do
+
+  it "should return a JSON object of an existing fact" do
     fact = Fact.new(name: "VAT rate", description: "The national VAT rate",
                     value: "20%", slug: "vat-rate")
     fact.save
 
     visit "/facts/vat-rate"
+    fact_response = JSON.load(page.body)
 
     page.status_code.should == 200
-    page.body.should have_content(fact.name)
-    page.body.should have_content(fact.slug)
-    page.body.should have_content(fact.description)
-    page.body.should have_content(fact.value)
+    fact_response.should == {
+      "_response_info" => {"status"=>"ok"},
+      "id" => "vat-rate",
+      "details" => {
+        "description" => "The national VAT rate",
+        "value" => "20%"
+      },
+      "title" => "VAT rate",
+      "updated_at" => fact.updated_at.xmlschema
+    }
   end
 end
