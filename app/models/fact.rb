0a5763a3adfa017d44b0cec24a8b24e32f55ac8e
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-09-09 12:58:59 +0000

    Refactor formatting methods into a module

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,19 +1,12 @@
 # encoding: UTF-8
+require 'formattable'
 
 class Fact
   include Mongoid::Document
   include Mongoid::Timestamps
-  include ActionView::Helpers::NumberHelper
+  include Formattable
 
   DATA_TYPES = [:currency, :date, :numeric, :text]
-  NUMERIC_FORMATS = { :percentage => "%d%" }
-  
-  CURRENCY_SYMBOLS = {
-    :eur => "€",
-    :gbp => "£",
-    :usd => "$"
-  }
-
 
   field :name, :type => String
   field :slug, :type => String
@@ -30,42 +23,5 @@ class Fact
   validates_presence_of :name, :value, :slug
   validates_inclusion_of :data_type, :in => DATA_TYPES.map(&:to_s)
   validates :slug, :uniqueness => true, :format => {:with => /\A[a-z0-9-]*\z/}
-
-  def formatted_value
-    send(:"formatted_#{data_type}_value")
-  end
-
-  def self.currency_codes
-    @currency_codes ||= YAML.load(File.open("lib/data/iso_4217_currency_codes.yml").read)  
-  end
-  
-  private
-
-  def formatted_currency_value
-    currency_symbol = CURRENCY_SYMBOLS[currency_code.downcase.to_sym]
-    amount = number_with_delimiter(value)
-    if currency_symbol
-      "#{currency_symbol}#{amount}"
-    else
-      "#{amount} #{self.class.currency_codes.key(currency_code)}"
-    end
-  end
-
-  def formatted_date_value
-    Date.parse(value).strftime("%Y-%m-%d")
-  end
-
-  def formatted_numeric_value
-    if numeric_format.nil?
-      number_with_delimiter(value)
-    else
-      format = NUMERIC_FORMATS[numeric_format.to_sym]
-      number_with_delimiter(sprintf(format, value))
-    end
-  end
-
-  def formatted_text_value
-    value
-  end
 
 end
--- spec/models/fact_spec.rb
+++ spec/models/fact_spec.rb
@@ -54,63 +54,11 @@ describe Fact do
       expect(fact).to have(1).error_on(:data_type)
     end
   end
-  describe "formatted_value" do
-    let(:fact) { FactoryGirl.build(:fact) }
-    describe "for currency data type" do
-      it "should default to Pound Sterling" do
-        fact.value = '99'
-        fact.data_type = 'currency'
-        expect(fact.currency_code).to eq("GBP")
-      end
-      it "should use the currency symbol when present" do
-        fact.value = '10000'
-        fact.data_type = 'currency'
-        fact.currency_code = 'EUR'
-        expect(fact.formatted_value).to eq("€10,000")
-      end
-      it "should use the name as a suffix if no currency symbol is present" do
-        fact.value = '1000'
-        fact.data_type = 'currency'
-        fact.currency_code = 'ZAR'
-        expect(fact.formatted_value).to eq("1,000 Rand")
-      end
-    end
-    describe "for date data type" do
-      it "should format the date as yyyy-MM-dd" do
-        fact.value = '7 Sept 2013'
-        fact.data_type = 'date'
-        expect(fact.formatted_value).to eq("2013-09-07")
-      end
-    end
-    describe "for numeric data type" do
-      it "should delimit the value with commas as per the style guide" do
-        fact.value = '10000000'
-        fact.data_type = 'numeric'
-        expect(fact.formatted_value).to eq("10,000,000")
-      end
-    end
-    it "should use formatting symbols where present" do
-      fact.value = '42'
-      fact.data_type = 'numeric'
-      fact.numeric_format = 'percentage'
-      expect(fact.formatted_value).to eq("42%")
-    end
-  end
+
   describe "data types" do
     it "should be an array of permitted types" do
       expect(Fact::DATA_TYPES).to eq([:currency, :date, :numeric, :text])
     end
   end
-  describe "numeric formats" do
-    it "should be a hash of numeric formats" do
-      expect(Fact::NUMERIC_FORMATS[:percentage]).to eq("%d%")
-    end
-  end
-  describe "currency codes data" do
-    it "should be a hash of ISO 4217 currency codes" do
-      expect(Fact.currency_codes["Euro"]).to eq("EUR")
-      expect(Fact.currency_codes["Pound Sterling"]).to eq("GBP")
-      expect(Fact.currency_codes["US Dollar"]).to eq("USD")
-    end
-  end
+
 end
